1034c20eaa90f3936fb41105be798a345aa3db6c
Edmund Kwan <edmund@promenadesoftware.com>
2019-06-05 08:47:49 -0700

    Update imports in Chores.js

--- src/components/Chores/Chores.js
+++ src/components/Chores/Chores.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import ChoresManager, {choresList, RotateAssignees} from '../../managers/ChoreManager';
+import {choresList, RotateAssignees, createData, rotatedAssignees} from '../../managers/ChoreManager';
 
 let id = 0;
 let weekLengthMS = 1000;//604800000;
