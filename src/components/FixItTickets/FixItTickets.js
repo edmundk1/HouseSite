9753c3e98aa66ca1e97c1aea068906775d9ff546
Edmund Kwan <edmund@promenadesoftware.com>
2019-07-21 16:27:18 -0700

    Remove unused import

--- src/components/FixItTickets/FixItTickets.js
+++ src/components/FixItTickets/FixItTickets.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
